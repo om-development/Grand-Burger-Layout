d873d0babbda6fac72691ffcdc95004ac66298df
Om Dhakal <omdhakal1019@gmail.com>
2026-06-07 21:27:17 +0545

    loader with i yeah

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import './style.css'
+window.addEventListener('load', () => {
+    setTimeout(() => {
+      document.getElementById('loader').classList.add('hidden');
+    }, 1800); // waits for logo animation to finish
+  });
