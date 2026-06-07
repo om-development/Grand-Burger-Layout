7755a0a6434f268b8ae81530a0130959d7bb7630
Om Dhakal <omdhakal1019@gmail.com>
2026-06-07 13:02:04 +0545

    vite js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        about: resolve(__dirname, 'about.html'),
+        order: resolve(__dirname, 'order.html'),
+      }
+    }
+  }
 })
